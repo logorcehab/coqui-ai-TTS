d12881bb1162416e1c47b537283ef2d21347b5d1
logorcehab <fiifinketiaa@gmail.com>
2026-04-05 08:55:09 +0000

    Implement get_user_data_dir function
    Add function to retrieve user data directory based on environment variables and platform.

--- TTS/utils/generic_utils.py
+++ TTS/utils/generic_utils.py
@@ -155,6 +155,26 @@ def setup_logger(
         sh.setFormatter(formatter)
         lg.addHandler(sh)
 
+def get_user_data_dir(appname):
+    TTS_HOME = os.environ.get("TTS_HOME")
+    XDG_DATA_HOME = os.environ.get("XDG_DATA_HOME")
+    if TTS_HOME is not None:
+        ans = Path(TTS_HOME).expanduser().resolve(strict=False)
+    elif XDG_DATA_HOME is not None:
+        ans = Path(XDG_DATA_HOME).expanduser().resolve(strict=False)
+    elif sys.platform == "win32":
+        import winreg  # pylint: disable=import-outside-toplevel
+
+        key = winreg.OpenKey(
+            winreg.HKEY_CURRENT_USER, r"Software\Microsoft\Windows\CurrentVersion\Explorer\Shell Folders"
+        )
+        dir_, _ = winreg.QueryValueEx(key, "Local AppData")
+        ans = Path(dir_).resolve(strict=False)
+    elif sys.platform == "darwin":
+        ans = Path("~/Library/Application Support/").expanduser()
+    else:
+        ans = Path.home().joinpath(".local/share")
+    return ans.joinpath(appname)
 
 def is_pytorch_at_least_2_4() -> bool:
     """Check if the installed Pytorch version is 2.4 or higher."""
